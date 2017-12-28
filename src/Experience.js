bb5ddcf429c1822ac5ca90548e5deecae7de17d8
jerrgree <jeremyrgreenburg@gmail.com>
2017-12-27 20:49:50 -0600

    Modified Experience Page

--- src/Experience.js
+++ src/Experience.js
@@ -5,35 +5,47 @@ import 'react-vertical-timeline-component/style.min.css';
 class Experience extends Component {
       render() {
             return (
-                <div className="content">
-                <h2>Experience</h2>
-                <VerticalTimeline>
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2017 - Present"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                >
-                <h3 className="vertical-timeline-element-title">Software Developer</h3>
-                <h4 classnMae="vertical-timeline-element-subtitle">Heartland Payment Systems</h4>
-                <h4 className="vertical-timeline-element-subtitle">Auburn, AL</h4>
-                <p>
-                .net C#, WCF, ASP.NET, Xamarin Forms
-                </p>
-                </VerticalTimelineElement>
+            <div className="content">
+            <h2>Experience</h2>
+            <VerticalTimeline>
+            <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2017 - Present"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            >
+            <h3 className="vertical-timeline-element-title">Software Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Heartland Payment Systems</h4>
+            <h4 className="vertical-timeline-element-subtitle">Auburn, AL</h4>
+            <p>
+            Skills: C#, SQLServer, WCF, ASP.NET, Xamarin Forms
+            </p>
+            <p>
+            Redeveloped the Bolletta Mobile application using Xamarin Forms.
+            </p>
+            <p>
+            Created a framework to host recurring payments using a variety of scheduled types, including an open ended recurring payment and scheduler to create new instances on demand.
+            </p>
+            </VerticalTimelineElement>
 
-                <VerticalTimelineElement
-                    className="vertical-timeline-element--work"
-                    date="2013 - 2017"
-                    iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-                >
-                <h3 className="vertical-timeline-element-title">B.S. Computer Science</h3>
-                <h4 className="vertical-timeline-element-subtitle">University of Tennessee at Martin</h4>
-                <h4 className="vertical-timeline-element-subtitle">Martin, TN</h4>
-                <p>
-                C++, Windows, Linux, Algorithms and Automata Theory, Artifical Intelligence, Software Development Lifecycles
-                </p>
-                </VerticalTimelineElement>
-                </VerticalTimeline>
+            <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2013 - 2017"
+            iconStyle={{ background: 'rgb(30, 233, 99)', color: '#fff' }}
+            >
+            <h3 className="vertical-timeline-element-title">B.S. Computer Science</h3>
+            <h4 className="vertical-timeline-element-subtitle">University of Tennessee at Martin</h4>
+            <h4 className="vertical-timeline-element-subtitle">Martin, TN</h4>
+            <p>
+            Skills: C, C++, R, Python, on both Windows and Linux
+            </p>
+            <p>
+            Graduated Summa Cum Laude
+            </p>
+            <p>
+            Took courses on Data Mining, Artificial Intelligence, Software Development Life Cycles, and Robotics
+            </p>
+            </VerticalTimelineElement>
+            </VerticalTimeline>
             </div>
         );
     }
